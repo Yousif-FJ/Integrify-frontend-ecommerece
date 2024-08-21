619d75b179b024568dfabd730414296a17d9050f
Yousif FJ <47510107+Yousif-FJ@users.noreply.github.com>
2024-08-21 18:14:38 +0300

    Initial changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import { Home } from "./pages/home"
+import './App.css'
+import { Link, Outlet } from 'react-router-dom'
 
 function App() {
+
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <>
+      <nav className='flex bg-gray-800 text-white top-0 py-3 flex-wrap justify-around bg-silver'>
+        <h1 className="text-lg font-semibold">e-commerce</h1>        
+        <ul className='flex gap-[40px] text-m'>
+          <li>
+            <Link to={'home'}>Home</Link>
+          </li>
+          <li>
+            <Link to={'orders'}>Orders</Link>
+          </li>
+          <li>
+            <Link to={'categories'}>Categories</Link>
+          </li>
+          <li>
+            <Link to={'users'}>Users</Link>
+          </li>
+          <li>
+            <Link to={'login'}>Login</Link>
+          </li>
+          <li>
+            <Link to={'register'}>Register</Link>
+          </li>
+        </ul>
+      </nav>
+      <Outlet></Outlet>
+    </>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,36 @@
-import ReactDOM from "react-dom/client"
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 
-import App from "./App"
-import "./index.css"
+import App from './App.tsx'
+import './index.css'
+import Home from './pages/home.tsx';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
-const queryClient = new QueryClient()
 
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-  <QueryClientProvider client={queryClient}>
-    <App />
-  </QueryClientProvider>
+const queryClient = new QueryClient();
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+    children: [
+      {
+        path: "",
+        element: <Home></Home>
+      }
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>,
 )
