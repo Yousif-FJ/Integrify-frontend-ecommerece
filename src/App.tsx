1845fcd3dacb3d3d72b5cb29f3199e5d39398135
Yousif FJ <47510107+Yousif-FJ@users.noreply.github.com>
2024-08-28 10:56:56 +0300

    Implement local storage for Auth state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import { Link, Outlet } from 'react-router-dom'
 import AuthUser from './types/AuthUser';
 
 
 export const AuthContext = createContext<AuthUser>({});
-export const AuthSetterContext = 
-        createContext<React.Dispatch<React.SetStateAction<AuthUser>>>(() => { });
+export const AuthSetterContext =
+  createContext<React.Dispatch<React.SetStateAction<AuthUser>>>(() => { });
+
+
+function getInitialAuthState() {
+  const authUserStr = localStorage.getItem('authState');
+
+  if (authUserStr) {
+    return JSON.parse(authUserStr) as AuthUser;
+  } else {
+    return {};
+  }
+}
 
 function App() {
 
-  const [authState, setAuthState] = useState<AuthUser>({});
+  const [authState, setAuthState] = useState<AuthUser>(getInitialAuthState());
+
+  useEffect(() => {
+    localStorage.setItem('authState', JSON.stringify(authState));
+  }, [authState]);
 
   return (
     <>
